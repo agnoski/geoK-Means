5d663097b5c7dcfcda7991b7a826e956824886df
agno <crash.pc89@gmail.com>
2020-03-28 13:50:07 +0100

    Get multiple geoapi requests

--- src/js/app.js
+++ src/js/app.js
@@ -8,21 +8,17 @@ function runApp() {
 
 function processAddresses() {
 	const addressesListText = $("#addressesList").val();
-	console.log(addressesListText);
 	const addressesList = addressesListText.split("\n");
-	console.log(addressesList);
 	const addresses = addressesList.map(address => address.split(","))
 									.filter(parts => parts.length === 3)
 									.map(element => {
 										return {street: element[0], number: element[1], city: element[2]};
 									});
 
-	console.log(addresses);
-
 	const url = "https://geocode.xyz/";
-	addresses.forEach( address => {
-		let urlReq = `${url}${address.street},${address.number}+${address.city}?json=1`;
-		console.log(urlReq);
-		$.getJSON(urlReq, data => console.log(data));
-	});
+
+	const urlReqs = addresses.map(address => `${url}${address.street},${address.number}+${address.city}?json=1`);
+	console.log(urlReqs);
+	const urlReqPromises = urlReqs.map(urlReq => $.getJSON(urlReq));
+	Promise.all(urlReqPromises).then(res => console.log(res)).catch(error => console.log(error));
 }
