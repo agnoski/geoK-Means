55095d1c11af422fb85c04ec3ed21ee8031b8962
agno <crash.pc89@gmail.com>
2020-03-28 20:48:08 +0100

    Refactor common functions

--- src/js/app.js
+++ src/js/app.js
@@ -6,19 +6,35 @@ function runApp() {
 	console.log("Ready!")
 }
 
+const url = "https://geocode.xyz/";
+const clusters = 2;
+
+const splitCSVAddress = addressText => addressText.split(",");
+const validateAddress = address => address.length === 3;
+const generateAddressObject = element => {
+	const address = {};
+	[address.street, address.number, address.city] = element;
+	return address;
+};
+const formatUrlReq = address => `${url}${address.street},${address.number}+${address.city}?json=1`;
+
 function processAddresses() {
 	const addressesListText = $("#addressesList").val();
 	const addressesList = addressesListText.split("\n");
-	const addresses = addressesList.map(address => address.split(","))
-									.filter(parts => parts.length === 3)
-									.map(element => {
-										return {street: element[0], number: element[1], city: element[2]};
-									});
+	const addresses = addressesList.map(splitCSVAddress)
+									.filter(validateAddress)
+									.map(generateAddressObject);
 
-	const url = "https://geocode.xyz/";
-
-	const urlReqs = addresses.map(address => `${url}${address.street},${address.number}+${address.city}?json=1`);
+	const urlReqs = addresses.map(formatUrlReq);
 	console.log(urlReqs);
 	const urlReqPromises = urlReqs.map(urlReq => $.getJSON(urlReq));
-	Promise.all(urlReqPromises).then(res => console.log(res)).catch(error => console.log(error));
+	Promise.all(urlReqPromises).then(res => {
+		console.log(res);
+		var coords = res.map(data => [parseFloat(data.latt), parseFloat(data.longt)]);	
+		var kmeans = new KMeans({
+			canvas: document.getElementById('plot'),
+			data: coords,
+			k: clusters
+		});
+	}).catch(error => console.log(error));
 }
