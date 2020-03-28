4d5252d24c166463751d58eeeb1f326776315f74
agno <crash.pc89@gmail.com>
2020-03-29 00:50:30 +0100

    Add delayed requests

--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,11 @@ function runApp() {
 
 const url = "https://geocode.xyz/";
 const clusters = 2;
+const delay = 1500;
 
 const splitCSVAddress = addressText => addressText.split(",");
 const validateAddress = address => address.length === 3;
 const generateAddressObject = element => {
-
 	const address = {};
 	[address.street, address.number, address.city] = element;
 	return address;
@@ -51,8 +51,16 @@ function processAddresses() {
 
 	const urlReqs = addresses.map(formatUrlReq);
 	console.log(urlReqs);
-	const urlReqPromises = urlReqs.map(urlReq => $.getJSON(urlReq));
-	Promise.all(urlReqPromises).then(res => {
+
+	const urlReqTimesPromise = urlReqs.map((urlReq, i) => {
+		return new Promise(resolve => setTimeout(resolve, i*delay)).then(() => {
+			console.log(`json call: ${urlReq}`);
+			return $.getJSON(urlReq);
+		});
+	});
+
+	Promise.all(urlReqTimesPromise).then(res => {
+		console.log("Success");
 		console.log(res);
 		var coords = res.map(data => [parseFloat(data.latt), parseFloat(data.longt)]);	
 		var kmeans = new KMeans({
@@ -67,5 +75,8 @@ function processAddresses() {
 		console.log(clustersList);
 
 		appendCluterslistHTML(clustersList);
-	}).catch(error => console.log(error));
+	}).catch(error => {
+		console.log("Error");
+		console.log(error);
+	});
 }
