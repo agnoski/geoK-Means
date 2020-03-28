7d84f772e4004f306d8854347016779b6affc5fb
agno <crash.pc89@gmail.com>
2020-03-28 22:28:57 +0100

    Add clutersList

--- src/js/app.js
+++ src/js/app.js
@@ -12,11 +12,23 @@ const clusters = 2;
 const splitCSVAddress = addressText => addressText.split(",");
 const validateAddress = address => address.length === 3;
 const generateAddressObject = element => {
+
 	const address = {};
 	[address.street, address.number, address.city] = element;
 	return address;
 };
 const formatUrlReq = address => `${url}${address.street},${address.number}+${address.city}?json=1`;
+
+const getClusterList = (assignments, adresses) => {
+	const clustersList = [];
+	assignments.forEach((c, i) => {
+		if(clustersList[c] === undefined) {
+			clustersList[c] = [];
+		}
+		clustersList[c].push(adresses[i]);
+	});
+	return clustersList;
+}
 
 function processAddresses() {
 	const addressesListText = $("#addressesList").val();
@@ -36,5 +48,10 @@ function processAddresses() {
 			data: coords,
 			k: clusters
 		});
+		console.log(kmeans.getAssignments());
+		console.log(kmeans.getMeans());
+
+		const clustersList = getClusterList(kmeans.getAssignments(), res);
+		console.log(clustersList);
 	}).catch(error => console.log(error));
 }
